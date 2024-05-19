c4952dee6dbf94990b5ec4f198f94a5e7d389d87
Sabeth <sabethpathen@gmail.com>
2024-05-19 15:35:48 -0400

    changed font

--- app.vue
+++ app.vue
@@ -29,3 +29,8 @@ export default {
     },
 };
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap");
+@import "assets/styles/body.css";
+</style>
