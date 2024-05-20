33a89cb6d620f531d3b70fc78480d74a3b9c3c7e
Sabeth <sabethpathen@gmail.com>
2024-05-19 21:07:49 -0400

    fixed

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/sabeth/Documents/purserenity/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/sabeth/Documents/purserenity/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
